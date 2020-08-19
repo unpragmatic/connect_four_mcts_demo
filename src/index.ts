6231228af7e60d13100ecec2be989dda6507918b
Callum Li <callum@callum.li>
2020-08-19 19:23:01 +1200

    Deploy

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { GameComponent } from './GameComponent';
-import * as ReactDOM from 'react-dom';
-import * as React from 'react';
-
-console.log("Hello world!");
-
-const gameNode = document.getElementById("game");
-ReactDOM.render(React.createElement(GameComponent, null), gameNode);
